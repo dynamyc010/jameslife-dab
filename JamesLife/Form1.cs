70f65031fc109d9f7ee4f221dc79e371a5b3f1a7
dynamyc | Bálint Sebestyén <33225256+dynamyc010@users.noreply.github.com>
2020-03-06 20:56:49 +0100

    Uploading the code and stuff

---
+++ JamesLife/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JamesLife
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Add("Welcome to JamesLife!");
+            listBox1.Items.Add("You are Championne De Pog. Age 0.");
+            listBox1.Items.Add("Father age 83.");
+            listBox1.Items.Add("Mother age 18.");
+            listBox1.Items.Add("----------------------------------------------------");
+            listBox1.Items.Add("What do you want to do?");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string command = textBox1.Text;
+            if (command == "surrender")
+            {
+                this.Close();
+            }
+            if(command == "help")
+            {
+                MessageBox.Show("In the alpha, we have one command. Surrender.\n(actually we have help too lol)");
+                return;
+            }
+            if(command == "dab")
+            {
+                dab openDab = new dab();
+                openDab.ShowDialog();
+                return;
+            }
+            MessageBox.Show("Command not recognized.");
+        }
+    }
+}
---
+++ JamesLife/dab.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JamesLife
+{
+    public partial class dab : Form
+    {
+        public dab()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("iexplore.exe", "https://dabme.me/");
+            Process.Start(startInfo);
+        }
+    }
+}
